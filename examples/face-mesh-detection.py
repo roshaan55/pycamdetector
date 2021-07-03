60d2b5a4f31e113b6979631adcf232ff9772d3e3
roshaan55 <46756917+roshaan55@users.noreply.github.com>
2021-07-03 08:40:18 +0500

    Create face-mesh-detection.py

---
+++ examples/face-mesh-detection.py
@@ -0,0 +1,20 @@
+import cv2
+import pycamdetector as pcam
+import time
+
+cap = cv2.VideoCapture(0)
+detector = pcam.FaceMeshDetector(maxFaces=1, minDetectionCon=0.7)
+# cap.set(3, wCam)
+# cap.set(4, hCam)
+pTime = 0
+while True:
+    success, img = cap.read()
+    img, faces = detector.findFaceMesh(img)
+    if len(faces) != 0:
+        print(faces[0])
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
