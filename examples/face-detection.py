3bd13a88bd4afc1cde3275c198ba380b382c34c0
roshaan55 <46756917+roshaan55@users.noreply.github.com>
2021-07-03 08:38:47 +0500

    Create face-detection.py

---
+++ examples/face-detection.py
@@ -0,0 +1,17 @@
+import cv2
+import pycamdetector as pcam
+import time
+
+cap = cv2.VideoCapture(0)
+pTime = 0
+detector = pcam.FaceDetector(minDetectionCon=0.7)
+while True:
+    success, img = cap.read()
+    img, bboxs = detector.findFaces(img)
+    print(bboxs)
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+    cv2.imshow('Pycamdetector', img)
+    cv2.waitKey(1)
