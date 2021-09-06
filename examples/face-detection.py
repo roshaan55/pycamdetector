d6c85df1731ca7bee232cb4f1630a8bae660240c
Roshaan Mehmood <46756917+roshaan55@users.noreply.github.com>
2021-09-06 15:18:55 +0500

    Updated face-detection.py File

--- examples/face-detection.py
+++ examples/face-detection.py
@@ -4,14 +4,16 @@ import time
 
 cap = cv2.VideoCapture(0)
 pTime = 0
-detector = pycamdetector.FaceDetector(minDetectionCon=0.7)
+detector = FaceDetector(minDetectionCon=0.85)
 while True:
     success, img = cap.read()
-    img, bboxs = detector.findFaces(img)
+    img = cv2.cv2.flip(img, 1)
+    img, bboxs = detector.findFaces(img, showPercentage=False)
     print(bboxs)
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
     cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
-    cv2.imshow('Pycamdetector', img)
-    cv2.waitKey(1)
+    cv2.imshow('MediaPipe Face Detection', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
