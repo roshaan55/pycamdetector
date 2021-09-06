ba50ae16f3509a975305910c91851db0ee039089
Roshaan Mehmood <46756917+roshaan55@users.noreply.github.com>
2021-09-06 15:29:59 +0500

    Updated pose-estimation.py File

--- examples/pose-estimation.py
+++ examples/pose-estimation.py
@@ -3,21 +3,22 @@ import time
 import pycamdetector
 
 cap = cv2.VideoCapture(0)
-detector = pycamdetector.poseDetector()
 pTime = 0
+detector = poseDetector()
 while True:
     success, img = cap.read()
-    img = detector.findPose(img, draw=False)
-    # img = cv2.flip(img, 1)
+    img = detector.findPose(img)
     lmList = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
         print(lmList[14])
+        cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 0), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
