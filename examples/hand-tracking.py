d77c92957a851b1b149422bccb68961f25a5ff69
Roshaan Mehmood <46756917+roshaan55@users.noreply.github.com>
2021-12-11 13:08:08 +0500

    Updated hand-tracking.py File

--- examples/hand-tracking.py
+++ examples/hand-tracking.py
@@ -1,28 +1,44 @@
 import cv2
 import pycamdetector as pcam
 
-pTime = 0
 cap = cv2.VideoCapture(0)
+FPSReader = pcam.FPS()  # Method to show FPS.
 detector = pcam.HandDetector(detectionCon=0.85, maxHands=1)
 
 while True:
     # Get image frame
     success, img = cap.read()
     # Find the hand and its landmarks
-    img = detector.findHands(img)
-    lmList, _ = detector.findPosition(img)
-    if len(lmList) != 0:
-        fingers = detector.fingersUp()
-        totalFingers = fingers.count(1)
-        print(totalFingers)
-    
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
+    hands, img = detector.findHands(img, flipType=False)  # with draw
+    # hands = detector.findHands(img, draw=False)  # without draw
 
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    if hands:
+        # Hand 1
+        hand1 = hands[0]
+        lmList1 = hand1["lmList"]  # List of 21 Landmark points
+        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
+        centerPoint1 = hand1['center']  # center of the hand cx,cy
+        handType1 = hand1["type"]  # Handtype Left or Right
+
+        fingers1 = detector.fingersUp(hand1)
+        print(fingers1)
+
+        if len(hands) == 2:
+            # Hand 2
+            hand2 = hands[1]
+            lmList2 = hand2["lmList"]  # List of 21 Landmark points
+            bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
+            centerPoint2 = hand2['center']  # center of the hand cx,cy
+            handType2 = hand2["type"]  # Hand Type "Left" or "Right"
+        
+            fingers2 = detector.fingersUp(hand2)
+        
+            # Find Distance between two Landmarks. Could be same hand or different hands
+            length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)  # with draw
+            # length, info = detector.findDistance(lmList1[8], lmList2[8])  # with draw
 
     # Display
+    fps, img = FPSReader.showFPS(img)  # display fps on screen
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
